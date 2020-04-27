e36a6104f0f9612ac6fd964d176086e4eaeec979
ChristopherCE <54681676+ChristopherCE@users.noreply.github.com>
2020-04-27 03:03:09 -0700

    Create header.h

---
+++ FINAL/header.h
@@ -0,0 +1,11 @@
+// -----------------------------------------------------------------------------
+// This is the header file for the project with all the libraries.
+// -----------------------------------------------------------------------------
+#include <iostream>     // Input - Output Library
+#include <iomanip>      // Various formatting
+#include <cstdlib>      // srand to set the seed
+#include <ctime>        // time for rand
+#include <fstream>      // needed for file operations
+#include <windows.h>    // for getasynkeystate function, Sleep();
+
+using namespace std;
