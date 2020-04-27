4cc16761b731d7a6d1a69b1b649616f13362a7e9
ChristopherCE <54681676+ChristopherCE@users.noreply.github.com>
2020-04-27 03:02:47 -0700

    Create record.h

---
+++ FINAL/record.h
@@ -0,0 +1,9 @@
+// -----------------------------------------------------------------------------
+// This header file holds a structure that is used for the high score file.
+// -----------------------------------------------------------------------------
+const int NAME_SIZE = 11;   // maximum size hi-score name can be (-1 for '/0')
+
+struct Record{
+    char name[NAME_SIZE];
+    int score;
+};
