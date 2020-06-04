7023a0afae8ae4d06f487a09141b3194d5ae2459
mperina11 <38636648+mperina11@users.noreply.github.com>
2020-06-04 16:53:09 -0700

    Update game.php

--- Web/game.php
+++ Web/game.php
@@ -1,19 +1,40 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <meta charset="utf-8">
+        <title>Space Invaders</title>
+        <link href='https://fonts.googleapis.com/css?family=Aldrich' rel='stylesheet'>
+        <link href="styles.css" rel="stylesheet">
+    </head>
     <body>
-        <form method="post" action="game.php">
-            Name: <input type="text" id="name" name="name" required><br><br>
-            Ship:
-                  <br><input type="radio" id="default" name="shipType" value="default" required>
-                  <label for="blue_choice">Blue</label><br>
-                  <input type="radio" id="default" name="shipType" value="default" required>
-                  <label for="green_choice">Green</label><br>
-                  <input type="radio" id="default" name="shipType" value="default" required>
-                  <label for="red_choice">Red</label><br>
-                  <input type="radio" id="default" name="shipType" value="default" required>
-                  <label for="yellow_choice">Yellow</label><br><br>
-            <input type="submit" value="Submit">
-        </form>
+    <h1 style="text-align:center;">SPACE INVADERS</h1>
+        <div id="container">
+            <div id="leaderboard">
+                <h3>Score:</h3>
+                <h3 id="score">0</h3>
+            </div>
+            <div id="board"></div>
+            <div id="lives">    
+                <h1>Lives: <span id="livesLeft">3</span></h1>
+            </div>
+            <div id="won">
+                <h1>Congratulations!</h1>
+                <h2>You won.</h2>
+                <br>
+                <button onclick="window.location.reload()">Restart</button>
+            </div>
+            <div id="lost">
+                <h1>Game over.</h1>
+                <h2>You lost.</h2>
+                <br>
+                <!-- <button onclick="window.location.reload()">Restart</button> -->
+                <form method="POST" action="leaderboard.php">
+                    <button type="submit" value=0>View Leaderboard</button>
+                </form>
+            </div>
+        </div>
+
+        <script src="app.js"></script>
 
         <!-- <?php
             $servername = "209.129.8.7";
@@ -21,23 +42,54 @@
             $password = "4050240368";
             $dbname = "RCCCSCCIS17B";
 
-            $nameP = $_GET['name'];
-
             // Create connection
             $conn = new mysqli($servername, $username, $password, $dbname);
             // Check connection
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
-            }
+            } 
 
-            $sql = "INSERT INTO spaceinvaders_entity_player (name)
-            VALUES ('nameP')";
+            // Query the Database
+            $sql = "SELECT `spaceinvaders_entity_player`.`name` AS `Player Name`, `spaceinvaders_enum_color`.`what_color` 
+            AS `Ship Color`, `spaceinvaders_entity_highscore`.`highscore` AS `High Score` 
+            FROM `RCCCSCCIS17B`.`spaceinvaders_xref_plyr_ship_highscore` AS `spaceinvaders_xref_plyr_ship_highscore`, `RCCCSCCIS17B`.`spaceinvaders_entity_player` 
+            AS `spaceinvaders_entity_player`, `RCCCSCCIS17B`.`spaceinvaders_entity_ship` 
+            AS `spaceinvaders_entity_ship`, `RCCCSCCIS17B`.`spaceinvaders_entity_highscore` 
+            AS `spaceinvaders_entity_highscore`, `RCCCSCCIS17B`.`spaceinvaders_enum_color` AS `spaceinvaders_enum_color` 
+            WHERE `spaceinvaders_xref_plyr_ship_highscore`.`plyr_id` = `spaceinvaders_entity_player`.`plyr_id` 
+            AND `spaceinvaders_xref_plyr_ship_highscore`.`ship_id` = `spaceinvaders_entity_ship`.`ship_id` 
+            AND `spaceinvaders_xref_plyr_ship_highscore`.`highscore_id` = `spaceinvaders_entity_highscore`.`highscore_id` 
+            AND `spaceinvaders_entity_ship`.`id_color` = `spaceinvaders_enum_color`.`id_ship` ORDER BY `High Score` DESC";
 
-            if ($conn->query($sql) === TRUE) {
-                echo "New record created successfully";
+            $result = $conn->query($sql);
+
+            echo "<table border='1'>";
+                echo "<tr>";
+                    echo "<th>".'Rank'."</th>";
+                    echo "<th>".'Player Name'."</th>";
+                    echo "<th>".'Ship Color'."</th>";
+                    echo "<th>".'High Score'."</th>";
+                echo "</tr>";
+
+            if ($result->num_rows > 0) 
+            {
+                $rank = 1;
+                // output data of each row
+                while($re = $result->fetch_assoc()) {
+                    echo "<tr>";
+                        echo "<td>".$rank."</td>";
+                        echo "<td>".$re["Player Name"]."</td>";
+                        echo "<td>".$re["Ship Color"]."</td>";
+                        echo "<td>".$re["High Score"]."</td>";
+                    echo "</tr>";
+                    $rank++;
+                }
             } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
+                echo "0 results";
             }
+            echo "</table>";
+
+            $conn->close();
         ?> -->
 
     </body>
