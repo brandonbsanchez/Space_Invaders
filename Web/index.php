91c4cf048320e829ba3c35437eab22c730118018
MattB <matthewborja97@gmail.com>
2020-06-03 21:04:56 -0700

    Updated form colors and images.

--- Web/index.php
+++ Web/index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <link href="styles.css" rel="stylesheet">
+    </head>
     <body>
-        <form method="post" action="game.php">
+    <form method="post" action="game.php">
             Name: <input type="text" id="name" name="name" required><br><br>
             Ship:
-                  <br><input type="radio" id="default" name="shipType" value="default" required>
-                  <label for="blue_choice">Blue</label><br>
-                  <input type="radio" id="default" name="shipType" value="default" required>
-                  <label for="green_choice">Green</label><br>
-                  <input type="radio" id="default" name="shipType" value="default" required>
-                  <label for="red_choice">Red</label><br>
-                  <input type="radio" id="default" name="shipType" value="default" required>
-                  <label for="yellow_choice">Yellow</label><br><br>
+                  <br><input type="radio" name="shipType" value="blue.png" required>
+                  <label for="blue_choice"><img class="icon" src="Images/blue.png">Blue</label><br>
+                  <input type="radio" name="shipType" value="green.png" required>
+                  <label for="green_choice"><img class="icon" src="Images/red.png">Green</label><br>
+                  <input type="radio" name="shipType" value="red.png" required>
+                  <label for="red_choice"><img class="icon" src="Images/red.png">Red</label><br>
+                  <input type="radio" name="shipType" value="orange.png" required>
+                  <label for="orange_choice"><img class="icon" src="Images/orange.png">Orange</label><br><br>
             <input type="submit" value="Submit">
         </form>
 
