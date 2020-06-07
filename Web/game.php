ad6f90eba22df07866c37acb23a0a2a40a52ae7e
mperina11 <38636648+mperina11@users.noreply.github.com>
2020-06-07 12:51:10 -0700

    Update game.php

--- Web/game.php
+++ Web/game.php
@@ -1,96 +1,100 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Space Invaders</title>
-        <link href='https://fonts.googleapis.com/css?family=Aldrich' rel='stylesheet'>
-        <link href="styles.css" rel="stylesheet">
-    </head>
-    <body>
-    <div id="Title" style="text-align:center;">SPACE INVADERS</div>
-        <div id="container">
-            <div id="leaderboard">
-                <div id="Score">Score:</div>
-                <div id="Num" id="score">0</div>
-            </div>
-            <div id="board"></div>
-            <div id="lives">    
-                <div class="Lives">Lives: <span id="livesLeft">3</span></div>
-            </div>
-            <div id="won">
-                <h1>Congratulations!</h1>
-                <h2>You won.</h2>
-                <br>
-                <button onclick="window.location.reload()">Restart</button>
-            </div>
-            <div id="lost">
-                <h1>Game over.</h1>
-                <h2>You lost.</h2>
-                <br>
-                <!-- <button onclick="window.location.reload()">Restart</button> -->
-                <form method="POST" action="leaderboard.php">
-                    <button type="submit" value=0>View Leaderboard</button>
-                </form>
-            </div>
-        </div>
-
-        <script src="app.js"></script>
-
-        <!-- <?php
-            $servername = "209.129.8.7";
-            $username = "RCCCSCCIS17B";
-            $password = "4050240368";
-            $dbname = "RCCCSCCIS17B";
-
-            // Create connection
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            // Check connection
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            } 
-
-            // Query the Database
-            $sql = "SELECT `spaceinvaders_entity_player`.`name` AS `Player Name`, `spaceinvaders_enum_color`.`what_color` 
-            AS `Ship Color`, `spaceinvaders_entity_highscore`.`highscore` AS `High Score` 
-            FROM `RCCCSCCIS17B`.`spaceinvaders_xref_plyr_ship_highscore` AS `spaceinvaders_xref_plyr_ship_highscore`, `RCCCSCCIS17B`.`spaceinvaders_entity_player` 
-            AS `spaceinvaders_entity_player`, `RCCCSCCIS17B`.`spaceinvaders_entity_ship` 
-            AS `spaceinvaders_entity_ship`, `RCCCSCCIS17B`.`spaceinvaders_entity_highscore` 
-            AS `spaceinvaders_entity_highscore`, `RCCCSCCIS17B`.`spaceinvaders_enum_color` AS `spaceinvaders_enum_color` 
-            WHERE `spaceinvaders_xref_plyr_ship_highscore`.`plyr_id` = `spaceinvaders_entity_player`.`plyr_id` 
-            AND `spaceinvaders_xref_plyr_ship_highscore`.`ship_id` = `spaceinvaders_entity_ship`.`ship_id` 
-            AND `spaceinvaders_xref_plyr_ship_highscore`.`highscore_id` = `spaceinvaders_entity_highscore`.`highscore_id` 
-            AND `spaceinvaders_entity_ship`.`id_color` = `spaceinvaders_enum_color`.`id_ship` ORDER BY `High Score` DESC";
-
-            $result = $conn->query($sql);
-
-            echo "<table border='1'>";
-                echo "<tr>";
-                    echo "<th>".'Rank'."</th>";
-                    echo "<th>".'Player Name'."</th>";
-                    echo "<th>".'Ship Color'."</th>";
-                    echo "<th>".'High Score'."</th>";
-                echo "</tr>";
-
-            if ($result->num_rows > 0) 
-            {
-                $rank = 1;
-                // output data of each row
-                while($re = $result->fetch_assoc()) {
-                    echo "<tr>";
-                        echo "<td>".$rank."</td>";
-                        echo "<td>".$re["Player Name"]."</td>";
-                        echo "<td>".$re["Ship Color"]."</td>";
-                        echo "<td>".$re["High Score"]."</td>";
-                    echo "</tr>";
-                    $rank++;
-                }
-            } else {
-                echo "0 results";
-            }
-            echo "</table>";
-
-            $conn->close();
-        ?> -->
-
-    </body>
-</html>
+body {
+    font-family: 'Aldrich';
+    margin: 0;
+    padding: 0;
+    background-image: linear-gradient(rgb(46, 46, 46), white );
+    background-repeat: no-repeat;
+    color: black;
+}
+#Title{
+    font-size: 30px;
+    color: white;
+}
+#S {
+    font-size: 20px;
+}
+#score {
+    font-size: 20px;
+    margin-left: 80px;
+    margin-top: -22px;
+    padding: 0; 
+}
+#container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height:80vh;
+}
+#board {
+    height: 600px;
+    width: 800px;
+    background-image: url(images/background.png);
+    animation: scroll 15s linear infinite;
+}
+#leaderboard{
+    color: white;
+    background-color: black;
+    height: 100px;
+    width: 800px;
+}
+#player {
+    position: absolute;
+    width: 40px;
+    margin-left: -20px; /*Moves center of object to middle*/
+}
+#lifeIcon{
+    width: 25px;
+    height: 25px;
+}
+.icon{
+    width: 25px;
+    height: 25px;
+}
+@keyframes scroll {
+    from {background-position-y: 0px;}
+    to {background-position-y: 1024px;}
+}
+.playerLaser {
+    position: absolute;
+    height: 20px;
+    margin-left: -3.9001px;
+}
+.enemyLaser {
+    position: absolute;
+    height: 20px;
+    margin-left: -10px;
+}
+.enemy {
+    position: absolute;
+    width: 40px;
+    margin-left: -20px;
+}
+#won {
+    display: none;
+    position: absolute;
+    background-color: rgb(17, 102, 14);
+    text-align: center;
+    border: solid 1px black;
+    padding: 25px;
+    border-radius: 30px;
+}
+#lost {
+    display: none;
+    position: absolute;
+    background-color: #862828;
+    text-align: center;
+    border: solid 1px black;
+    padding: 10px 50px;
+    border-radius: 30px;
+}
+#won button, #lost button {
+    padding: 10px;
+    border-radius: 5px;
+}
+#lives {
+    margin-left: -710px;
+    font-size: 20px;
+}
+tr:nth-child(odd){background-color: rgb(107, 107, 107);}
+tr:nth-child(even){background-color: rgb(153, 153, 153);}
