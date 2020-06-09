05bbe06160505fefa81d2866efc3b4c37c3dd021
MattB <matthewborja97@gmail.com>
2020-06-08 17:39:47 -0700

    Implemented regular expression for username.

--- Web/index.php
+++ Web/index.php
@@ -10,11 +10,18 @@
                 <div class="Title" style="text-align:center;">SPACE INVADERS</div>
                 <div class="Under" style="text-align:center;">*please enter your name and select a ship to start*</div>
                 <div class="Under" style="text-align:center;">*A = Left. D = Right. Spacebar = Shoot*</div>
-            Name: <?php if(isset($_COOKIE['name'])){
-                echo '<input type="text" id="name" name="name" value="' . $_COOKIE["name"] . '" required><br><br>';
-            } else{
-                echo '<input type="text" id="name" name="name" require><br><br>';
-            }?>
+            <?php
+            //Check for errors in username:
+            if(isset($_GET['error']) == 'invalidusername'){ //If invalidusername, alert user:
+                echo '<p id="error">Invalid entry: Enter only alpha-numeric characters.</p>';
+            } else{ 
+                echo 'Name:'; if(isset($_COOKIE['name'])){
+                    echo '<input type="text" id="name" name="name" value="' . $_COOKIE["name"] . '" required><br><br>';
+                } else{
+                    echo '<input type="text" id="name" name="name" require><br><br>';
+                }
+            }
+            ?>
             Ship:
                   <br><input type="radio" name="shipType" value="blue.png" required>
                   <label for="blue_choice"><img class="icon" src="Images/blue.png">Blue</label><br>
